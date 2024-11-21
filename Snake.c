06d09119f5eed4b09cf92b4a3b5f2004a574a4d9
LuisEduardo000 <luise.gonzalez@iteso.mx>
2024-11-20 21:54:57 -0600

    Reinicio de juego con el switch
    Se implemento función que según el valor del switch continua el juego o se apagan los leds y se reinicia la partida

--- Snake.c
+++ Snake.c
@@ -2,29 +2,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 volatile unsigned int *led_base = (unsigned int *)LED_MATRIX_0_BASE;
 volatile unsigned int *d_pad_up = (unsigned int *)D_PAD_0_UP;
 volatile unsigned int *d_pad_do = (unsigned int *)D_PAD_0_DOWN;
 volatile unsigned int *d_pad_le = (unsigned int *)D_PAD_0_LEFT;
 volatile unsigned int *d_pad_ri = (unsigned int *)D_PAD_0_RIGHT;
+volatile unsigned int *switch_0 = (unsigned int *)SWITCHES_0_BASE;
 
 int snakeHeadx, snakeHeady;
 int prevsnakex, prevsnakey;
 int appleX, appleY;
+int juegoActivo = 0;  // 0 = juego detenido, 1 = juego activo
+
+void apagarLEDs() {
+    for (int i = 0; i < LED_MATRIX_0_WIDTH * LED_MATRIX_0_HEIGHT; i++) {
+        led_base[i] = 0;  // Apagar LED
+    }
+}
 
 void startGame() {
+    apagarLEDs();  // Limpiar la pantalla antes de iniciar
     snakeHeadx = rand() % (LED_MATRIX_0_WIDTH - 1);
     snakeHeady = rand() % (LED_MATRIX_0_HEIGHT - 1);
-    //Revisar que el spawn sea en un lugar correcto
-    if (snakeHeadx % 2 != 0) {snakeHeadx -= 1;} //si no, ajustar
-    if (snakeHeady % 2 != 0) {snakeHeady -= 1;}
+
+    // Asegurarnos de que las coordenadas sean válidas (pares)
+    if (snakeHeadx % 2 != 0) { snakeHeadx--; }
+    if (snakeHeady % 2 != 0) { snakeHeady--; }
 
     prevsnakex = snakeHeadx;
     prevsnakey = snakeHeady;
 
     createSnake();
     spawnApple();
+    juegoActivo = 1;  // Activar el juego
 }
 
 void createSnake() {
@@ -35,7 +45,7 @@ void createSnake() {
     led_base[prev_index + LED_MATRIX_0_WIDTH + 1] = 0;
 
     int head_index = snakeHeady * LED_MATRIX_0_WIDTH + snakeHeadx;
-    led_base[head_index] = 0xFF0000;
+    led_base[head_index] = 0xFF0000;  // Color de la serpiente
     led_base[head_index + 1] = 0xFF0000;
     led_base[head_index + LED_MATRIX_0_WIDTH] = 0xFF0000;
     led_base[head_index + LED_MATRIX_0_WIDTH + 1] = 0xFF0000;
@@ -44,46 +54,59 @@ void createSnake() {
     prevsnakey = snakeHeady;
 }
 
-
 void updateSnake(int x, int y) {
     snakeHeadx += x;
     snakeHeady += y;
+
+    // Comprobar si la serpiente come la manzana
+    if (snakeHeadx == appleX && snakeHeady == appleY) {
+        spawnApple();  // Generar una nueva manzana
+    }
 }
 
 void spawnApple() {
     appleX = rand() % (LED_MATRIX_0_WIDTH - 1);
     appleY = rand() % (LED_MATRIX_0_HEIGHT - 1);
-    
-    //crea otra manzana si se genero en el mismo lugar que la cerpiente
+
+    // Asegurarnos de que las coordenadas sean válidas (pares)
+    if (appleX % 2 != 0) { appleX--; }
+    if (appleY % 2 != 0) { appleY--; }
+
+    // Asegurarnos de que no aparezca sobre la serpiente
     if (appleX == snakeHeadx && appleY == snakeHeady) {
         spawnApple();
-        return;}
-    
-    //Revisar que el spawn es en linea correcta
-    if (appleY % 2 != 1) {appleY -= 1;} //si no, ajustar
-    if (appleX % 2 != 1) {appleX -= 1;}
+        return;
+    }
 
     int apple_index = appleY * LED_MATRIX_0_WIDTH + appleX;
-    led_base[apple_index] = 0x00FF00;
+    led_base[apple_index] = 0x00FF00;  // Color de la manzana
     led_base[apple_index + 1] = 0x00FF00;
     led_base[apple_index + LED_MATRIX_0_WIDTH] = 0x00FF00;
     led_base[apple_index + LED_MATRIX_0_WIDTH + 1] = 0x00FF00;
 }
 
-
 void main() {
-    int x = 2, y = 0;  
-    startGame();
-    
+    int x = 2, y = 0;
+
     while (1) {
-        if (*d_pad_up && y == 0) { x = 0; y = -2; }  
-        else if (*d_pad_do && y == 0) { x = 0; y = 2; } 
-        else if (*d_pad_le && x == 0) { x = -2; y = 0; } 
-        else if (*d_pad_ri && x == 0) { x = 2; y = 0; }  
+        if (*switch_0 == 1) {  // Si el switch está activado
+            juegoActivo = 0;   // Detener el juego
+            apagarLEDs();
+            continue;        
+        } else if (juegoActivo == 0) {  // Si el juego estaba detenido y el switch se apagó
+            startGame();  // Reiniciar el juego
+        }
 
-        updateSnake(x, y);
-        createSnake();
+        if (juegoActivo) {  // Solo actualizar si el juego está activo
+            if (*d_pad_up && y == 0) { x = 0; y = -2; }
+            else if (*d_pad_do && y == 0) { x = 0; y = 2; }
+            else if (*d_pad_le && x == 0) { x = -2; y = 0; }
+            else if (*d_pad_ri && x == 0) { x = 2; y = 0; }
 
-        for (int i = 0; i <= 10000; i++);
+            updateSnake(x, y);
+            createSnake();
+        }
+
+        for (int i = 0; i <= 10000; i++);  // Retardo
     }
 }
